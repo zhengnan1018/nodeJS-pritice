0e21111e09a5677529de71e0ea077dd15fadfb6b
jessy.zheng@pwc.com <jessy.zheng@pwc.com>
2016-07-08 14:15:24 +0800

    add a example how to user string templates

--- express_application/router.js
+++ express_application/router.js
@@ -1,7 +1,12 @@
 const routers = (app) => {
-  console.log(app);
-  // let express = require('../node_modules/express') //加载 express
-  // let app = express() //实例化一个 express 应用
+
+  var testCallback1 = () => {
+    console.log('I am callback1');
+  }
+  var testCallback2 = () => {
+    console.log('I am callback2');
+  }
+
   let appGet =
     app
       //在 express 上监听 get 请求
@@ -10,6 +15,12 @@ const routers = (app) => {
         //response.send([body]) 发送响应文件
         //发送http 响应，里面的[body]参数可以是一个buffer 字符串 对象 或者数组（会转成JSON 格式响应）
       })
+      // all() 方法的作用是对于一个路径上的所有请求加载中间件
+      .all('/user', (request, response, next) => {
+        console.log('must be excutive in all methods');
+        next()
+      }, [testCallback1, testCallback2]) // 调用函数数组
+      // 执行时发现只有第一个回调可以执行...TODO...
       .get('/user', (request, response) => { //user 请求
         response.send('Express User')
       })
